cc1f3a1cb8498093e3f844c3dd5c8fa8734351dd
meetarora <meet.arora2403@gmail.com>
2024-07-29 01:37:44 +0530

    my-commit

---
+++ second.js
@@ -0,0 +1,26 @@
+let trackList = [
+    {
+      name: "Satnam Waheguru - Diljit Dosanjh",
+      path: "./music/sw.mp3",
+      img: "https://i.ytimg.com/vi/vxlJTSpusIM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDwOor0hxdlegnFEjQtgi16kadqDA",
+      singer: "Diljith Dosanjh",
+    },
+    {
+      name: "Mitar Pyaare Nu",
+      path: "./music/mn.mp3",
+      img: "https://i.ytimg.com/vi/-aPNfQYjFR8/maxresdefault.jpg",
+      singer: "",
+    },
+    {
+      name: "Shukar Dateya",
+      path: "./music/sd.webm",
+      img: "https://i.ytimg.com/vi/ABb56SmcWmU/maxresdefault.jpg",
+      singer: "Prabh Gill",
+    },
+    {
+      name: "Aar Nanak Par Nanak",
+      path: "./music/an.mp3",
+      img: "https://static.toiimg.com/thumb/msid-66688023,width-1280,resizemode-4/66688023.jpg",
+      singer: "Diljith Dosanjh",
+    },
+  ];
